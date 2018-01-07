b0cc3f0eb409dd419171151069c1e9250141af5e
worc <curtis.s.mcallister@gmail.com>
2018-01-07 15:51:53 -0800

    add stubs for serving the trinkets route

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import trinkets from './trinkets.mjs';
+import shuffle from './shuffle.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/trinkets", (req, res) => {
+    let trinketList = `<ul>`;
+
+    shuffle(trinkets);
+
+    for(let i = 0; i < 5; i++ ) {
+        let trinket = trinkets.pop();
+        trinketList += `<li>${trinket}</li>`;
+    }
+
+    trinketList += '</ul>';
+
+    res.status(200).send(`
+        ${trinketList}
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log("Server listening on", PORT);
+});
