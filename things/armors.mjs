2de45b4fc62569c5633f0f02ea81054c09936b02
worc <curtis.s.mcallister@gmail.com>
2018-01-19 14:27:35 -0800

    add some basic armor info from 5e basic rules

---
+++ things/armors.mjs
@@ -0,0 +1,81 @@
+export default [
+    {
+        name: 'breastplate',
+        type: 'medium armor',
+        cost: '400 gp',
+        weight: '20 lbs',
+        ac: 14
+    }, {
+        name: 'chain mail',
+        type: 'heavy armor',
+        cost: '75 gp',
+        weight: '55 lbs',
+        ac: 16
+    }, {
+        name: 'chain shirt',
+        type: 'medium armor',
+        cost: '50 gp',
+        weight: '20 lbs',
+        ac: 13
+    }, {
+        name: 'half plate',
+        type: 'medium armor',
+        cost: '750 gp',
+        weight: '40 lbs',
+        ac: 15
+    }, {
+        name: 'hide',
+        type: 'medium armor',
+        cost: '10 gp',
+        weight: '12 lbs',
+        ac: 12
+    }, {
+        name: 'leather',
+        type: 'light armor',
+        cost: '10 gp',
+        weight: '10 lbs',
+        ac: 11,
+    }, {
+        name: 'padded',
+        type: 'light armor',
+        cost: '5 gp',
+        weight: '8 lbs',
+        ac: 11
+    }, {
+        name: 'plate',
+        type: 'heavy armor',
+        cost: '1500 gp',
+        weight: '65 lbs',
+        ac: 18
+    }, {
+        name: 'ring mail',
+        type: 'heavy armor',
+        cost: '30 gp',
+        weight: '40 lbs',
+        ac: 14
+    }, {
+        name: 'scale mail',
+        type: 'medium armor',
+        cost: '50 gp',
+        weight: '45 lbs',
+        ac: 14
+    }, {
+        name: 'spiked armor',
+        type: 'medium armor',
+        cost: '75 gp',
+        weight: '45 lbs',
+        ac: 14
+    }, {
+        name: 'splint',
+        type: 'heavy armor',
+        cost: '200 gp',
+        weight: '60 lbs',
+        ac: 17
+    }, {
+        name: 'studded leather',
+        type: 'light armor',
+        cost: '45 gp',
+        weight: '13 lbs',
+        ac: 12
+    }
+];
