42017d951f678991ce85d8b86cc3e8eb867e5b09
worc <curtis.s.mcallister@gmail.com>
2018-01-25 14:40:13 -0800

    import path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import React from 'react';
 import renderToString from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import path from 'path';
 
 import App from './shared/App';
 
