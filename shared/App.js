f0892a97149578f8afa11a05f40615f9c61f8831
worc <curtis.s.mcallister@gmail.com>
2018-01-29 17:59:05 -0800

    remove dead code

--- shared/App.js
+++ shared/App.js
@@ -3,9 +3,6 @@ import React from "react";
 import Sidebar from './Sidebar';
 import Main from './Main';
 
-// import Glyphtionary from "../shared/components/Glyphtionary";
-// import GlyphSequence from "../shared/components/GlyphSequence";
-
 class App extends React.Component{
     render() {
         return (
