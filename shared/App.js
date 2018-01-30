5f8269397aaa9fa40df7fdac2fae00b49f1803fd
worc <curtis.s.mcallister@gmail.com>
2018-01-30 15:30:30 -0800

    add more flexbox styling to enable sticky footer

--- shared/App.js
+++ shared/App.js
@@ -7,10 +7,16 @@ import Main from './Main';
 class App extends React.Component{
     render() {
         return (
-            <div>
+            <div style={{
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexFlow: 'column nowrap',
+                height: '100%',
+                padding: '0px'
+            }}>
                 <Header />
                 <Sidebar />
-                <Main />
+                <Main style={{ display: 'flex', flexFlow: 'column', flex: '1 0 auto' }} />
             </div>
         )
     }
--- shared/Main.js
+++ shared/Main.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 import Trinkets from './Trinkets';
 import MinorMagic from './minor-magic/_MinorMagic';
 
-export default () => {
+export default (props) => {
     return (
-        <Switch>
-            <Route path='/trinkets' exact component={ Trinkets } />
-            <Route path='/minor-magic/:type?' component={ MinorMagic } />
-        </Switch>
+        <div style={props.style}>
+            <Switch>
+                <Route path='/trinkets' exact component={ Trinkets } />
+                <Route path='/minor-magic/:type?' component={ MinorMagic } />
+            </Switch>
+        </div>
     )
 }
