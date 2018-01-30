ee64ad370bd42863e732386e4c7b9a660438b6ba
worc <curtis.s.mcallister@gmail.com>
2018-01-29 17:55:31 -0800

    add sidebar routing to show subnav for minor magic links

--- shared/Sidebar.js
+++ shared/Sidebar.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
 export default () => {
     return (
         <div>
             <NavLink to="/trinkets">Trinkets</NavLink>
+            <NavLink to='/minor-magic'>Minor Magic</NavLink>
+            <Switch>
+                <Route path='/minor-magic' render={ () => {
+                        return (
+                            <div>
+                                <NavLink to='/minor-magic/armor'>Armor</NavLink>
+                                <NavLink to='/minor-magic/item'>Item</NavLink>
+                                <NavLink to='/minor-magic/weapon'>Weapon</NavLink>
+                            </div>
+                        )
+                    }
+                } />
+
+            </Switch>
         </div>
     )
 }
