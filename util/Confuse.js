68464f4af887bc83953ede103e35a82a74b75e6c
worc <curtis.s.mcallister@gmail.com>
2018-02-02 16:38:37 -0800

    add updates to Confuse and Strategy from the tester mjs files

--- util/Confuse.js
+++ util/Confuse.js
@@ -1,4 +1,5 @@
 import { getFromShuffled, drainFromShuffled } from './Generators';
+import Strategy from './Strategy';
 
 /**
  * Confuse.js
@@ -11,29 +12,6 @@ import { getFromShuffled, drainFromShuffled } from './Generators';
  * @licence MIT
  */
 
-function obfuscateOne(message = '', bitmap = [], characterGenerator, exclude = []) {
-    let workingArray = message.split('');
-    let targetIndex = Math.floor(Math.random() * workingArray.length);
-
-    if(bitmap[targetIndex] === 1 && !exclude.includes(workingArray[targetIndex])) {
-        workingArray[targetIndex] = characterGenerator.next().value;
-    }
-
-    return workingArray.join('');
-}
-
-function obfuscateAll(message = '', bitmap = [], characterGenerator, exclude = []) {
-    let splitMessage = message.split('');
-
-    return bitmap.map((bit, index) => {
-        if(bit === 1 && !exclude.includes(splitMessage[index])) {
-            return characterGenerator.next().value;
-        } else {
-            return splitMessage[index];
-        }
-    }).join('');
-}
-
 export default class Confuse {
     /**
      *
@@ -41,83 +19,129 @@ export default class Confuse {
      * @param resolution
      * @param options
      */
-    constructor(listener = (message) => { console.log(message) }, resolution = '', options = {
-        characters: 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz~!@#$%^&*()-+=[]{}|;:,./<>?',
-        exclude: ' ',
-        startBaffled: true,
-        speed: 50,
-    }) {
-        this.options = { ...options };
+
+    constructor(
+        listener = (message) => { console.warn('no handler given for message:', message) },
+        resolution = '',
+        {
+            characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz~!@#$%^&*()-+=[]{}|;:,./<>?',
+            exclude = ' ',
+            startBaffled = true,
+            speed = 50,
+        }
+    ) {
+        this.options = {
+            characters,
+            exclude,
+            startBaffled,
+            speed
+        };
         this.listener = listener;
         this.resolution = resolution;
-        this.baffling = resolution;
+        this.output = resolution;
 
         this.options.exclude = this.options.exclude.split('');
-        console.log(this.options.exclude);
         this.options.characters = this.options.characters.split('');
         this.characterGenerator = getFromShuffled(this.options.characters);
         this.bitmap = this.resolution.split('').map(() => (this.options.startBaffled) ? 1 : 0 );
 
-        this.once();
+        this.render(this.bitmap);
+
+        //todo parameterize:
+        this.strategy = Strategy.obfuscateOneBitAndShuffleUntilDone(this.bitmap);
     }
 
     step() {
-        this.baffling = obfuscateOne(this.baffling, this.bitmap, this.characterGenerator, this.options.exclude);
-        this.listener(this.baffling);
+        const { value: bitmap, done } = this.strategy.next();
+
+        console.log('done', done);
+        console.log('value', bitmap);
+
+        if(done) {
+            this.pause();
+        }
+
+        this.render(bitmap);
         return this;
     }
 
-    once() {
-        this.baffling = obfuscateAll(this.baffling, this.bitmap, this.characterGenerator, this.options.exclude);
-        this.listener(this.baffling);
-        return this;
-    }
-
-    loop() {
+    loop(duration, delay = 0) {
         clearInterval(this.interval);
         this.interval = setInterval(() => this.step(), this.options.speed);
+        if(duration) {
+            setTimeout(() => clearInterval(this.interval), duration);
+        }
         return this;
     }
 
-    pause() {
-        clearInterval(this.interval);
+    // todo duration param? store the cleared interval so it can be recovered?
+    pause(delay = 0) {
+        setTimeout(() => { clearInterval(this.interval); console.log('paused', delay) }, delay);
         return this;
     }
 
-    decay(duration = 0) {
-        // todo, if there's extra time, stall with looping
-        // todo, if there's not enough time use a faster pace
+    render(bitmap) {
+        // todo refactor to be functional instead of global/side-effect?
+        this.bitmap = bitmap;
+        this.output = this.bitmap.map((bit, index) => {
+            return (bit === 0) ? this.resolution.substring(index, index + 1) : this.characterGenerator.next().value ;
+        }).join('');
 
-        let cycles = duration / this.options.speed || 1;
-        let splitBaffling = this.baffling.split('');
-        let splitResolution = this.resolution.split('');
-        let onBitIndices = this.bitmap.map((bit, index) => { if(bit === 1) return index });
-        let indexGenerator = drainFromShuffled(onBitIndices);
-        let pace = onBitIndices.length / cycles;
+        this.emit();
 
-        // console.log('length', onBitIndices.length);
-        // console.log('duration', duration);
-        // console.log('cycles', cycles);
-        // console.log('pace', pace);
+        return this;
+    }
 
-        this.interval = setInterval(() => {
-            let targetIndex = indexGenerator.next().value;
+    emit() {
+        this.listener(this.output);
+        return this;
+    }
 
-            splitBaffling[targetIndex] = splitResolution[targetIndex];
-            this.bitmap[targetIndex] = 0;
-            this.baffling = splitBaffling.join('');
-            this.listener(this.baffling);
+    grow(duration = -1) {
+        this.strategy = Strategy.obfuscateOneBitAndShuffleUntilDone(this.bitmap);
+        this.loop();
+        return this;
+    }
 
-            if(onBitIndices.length === 0) {
-                this.pause();
-            }
-        }, 50)
+    decay(duration = -1) {
+        this.strategy = Strategy.revealOneBitAndShuffleUntilDone(this.bitmap);
+        this.loop();
+        return this;
+
+        // this.pause();
+        //
+        // // todo, if there's extra time, stall with looping
+        // // todo, if there's not enough time use a faster pace
+        //
+        // let cycles = duration / this.options.speed || 1;
+        // let splitBaffling = this.output.split('');
+        // let splitResolution = this.resolution.split('');
+        // let onBitIndices = this.bitmap.map((bit, index) => { if(bit === 1) return index });
+        // let indexGenerator = drainFromShuffled(onBitIndices);
+        // let pace = onBitIndices.length / cycles;
+        //
+        // // console.log('length', onBitIndices.length);
+        // // console.log('duration', duration);
+        // // console.log('cycles', cycles);
+        // // console.log('pace', pace);
+        //
+        // this.interval = setInterval(() => {
+        //     let targetIndex = indexGenerator.next().value;
+        //
+        //     splitBaffling[targetIndex] = splitResolution[targetIndex];
+        //     this.bitmap[targetIndex] = 0;
+        //     this.output = splitBaffling.join('');
+        //     this.listener(this.output);
+        //
+        //     if(onBitIndices.length === 0) {
+        //         this.pause();
+        //     }
+        // }, 50)
     }
 
     resolve(duration = 0, delay = 0) {
         setTimeout(() => {
-            console.log('resolving');
-            this.pause();
+            // this.pause();
             this.decay(duration);
         }, delay);
     }
