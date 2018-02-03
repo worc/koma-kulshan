584e3f7fdcb7b66d658cdda2034676ee7f6f7288
worc <curtis.s.mcallister@gmail.com>
2018-02-02 16:37:06 -0800

    add some notes in the tester file

--- mjs/test.mjs
+++ mjs/test.mjs
@@ -2,8 +2,20 @@ import Confuse from './Confuse.mjs';
 
 let starter = 'some kind of text';
 
-let confuser = new Confuse((message) => { console.log(message)}, starter);
+let confuser = new Confuse((message) => { console.log(message)}, starter, {
+    characters: ["█", "▓", "▒", "░", "█", "▓", "▒", "░", "█", "▓", "▒", "░", "<", ">", "/"].join(''),
+    // startBaffled: false
+    startBaffled: false
+});
+
+// let confuser = new Confuse(() => {}, 'some kind of text');
+
+// confuser.loop().pause(2000);
+
+// confuser.grow();
+
+// confuser.decay();
 
 confuser.loop();
 
-confuser.resolve(2500, 100);
+// confuser.resolve(2500, 100);
