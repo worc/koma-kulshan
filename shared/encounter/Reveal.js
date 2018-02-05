597ec25f86affacfa20fa3d22a6acac3238bd2cb
worc <curtis.s.mcallister@gmail.com>
2018-02-05 10:04:53 -0800

    add the Reveal component
    encounter details are 'revealed' in this component by using a custom reworking of the baffle library to obscure the text and then resolve it over time

---
+++ shared/encounter/Reveal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Confuse from '../../util/Confuse';
+
+const leadInStyle = {
+    fontFamily: '"Rammetto One", cursive'
+};
+
+const revealStyle = {
+    textAlign: 'right'
+};
+
+export default class Reveal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reveal: ''
+        }
+    }
+
+    listener(message) {
+        this.setState({ reveal: message });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const confuser = new Confuse(this.listener.bind(this), nextProps.reveal, {
+            // todo let Confuse handle arrays of characters
+            characters: ["█", "▓", "▒", "░", "█", "▓", "▒", "░", "█", "▓", "▒", "░", "<", ">", "/"].join(''),
+            exclude: '. ',
+            speed: 50
+        });
+
+        confuser.loop().resolve(1500);
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={ leadInStyle }>{ this.props.leadIn }...</div>
+                <div style={ revealStyle }>...{ this.state.reveal }</div>
+            </div>
+        )
+    }
+}
