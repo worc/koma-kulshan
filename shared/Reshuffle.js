417732ec6838b388b22ed458a88a2012926e5574
worc <curtis.s.mcallister@gmail.com>
2018-02-05 12:09:32 -0800

    factor out reshuffle buttons into a component

---
+++ shared/Reshuffle.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const containerStyle = {
+    cursor: 'pointer',
+    display: 'flex',
+    flex: '0 0 auto',
+    flexFlow: 'row wrap',
+    fontVariantCaps: 'all-small-caps',
+    letterSpacing: '0.2rem',
+    margin: '0 -10px',
+    width: '100%'
+};
+
+const buttonStyle = {
+    alignItems: 'center',
+    border: '1px solid black',
+    display: 'flex',
+    flex: '1 0 auto',
+    height: '4rem',
+    justifyContent: 'center'
+};
+
+const reshuffleStyle = {
+    backgroundColor: '#8d88df',
+    boxSizing: 'border-box',
+    color: '#fff',
+    flex: '1 0 100%'
+};
+
+const subShufflesGroupStyle = {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-around',
+    width: '100%'
+};
+
+const subShuffleButtonStyle = {
+    backgroundColor: '#ace',
+};
+
+export default ({reshuffleHandler, subShuffles}) => {
+    return (
+        <div style={ containerStyle } onClick={ reshuffleHandler }>
+            <div style={{ ...buttonStyle, ...reshuffleStyle }}><span>reshuffle</span></div>
+            <div style={ subShufflesGroupStyle }>
+                {
+                    subShuffles.map((sub, i) => {
+                        return (<div
+                                    key={i}
+                                    style={{ ...buttonStyle, ...subShuffleButtonStyle }}>
+                                    <span>{sub}</span>
+                                </div>)
+                    })
+                }
+            </div>
+        </div>
+    )
+}
