b7ac68f7770607d803ba2556c9d4fd76af2b554b
worc <curtis.s.mcallister@gmail.com>
2018-02-08 12:56:51 -0800

    add a stub for desert encounters

---
+++ encounters/in-the-desert.js
@@ -0,0 +1,18 @@
+// let example = [
+//     {
+//         generalDescription: 'individual',
+//         specific: [
+//             'long description',
+//             'also a description',
+//         ]
+//     }
+// ]
+
+export default [
+    {
+        generalDescription: 'an individual',
+        specific: [
+
+        ]
+    }
+]
