50898e307ccfb4bc62dc8e75eb652f923ec790a9
worc <curtis.s.mcallister@gmail.com>
2018-02-08 12:46:45 -0800

    add a reveal component where the hidden text can be toggled

---
+++ shared/TapToReveal.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Confuse from "../util/Confuse";
+
+export default class TapToReveal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            obfuscate: true,
+            reveal: '',
+            resolution: ''
+        };
+
+        this.confuser = new Confuse(this.listener.bind(this), this.props.reveal, {
+            characters: [
+                '▀▁▂▃▄▅▆▇█▉▊▋▌▍▎', // U+258x
+                '▐░▒▓▔▕▖▗▘▙▚▛▜▝▞▟', // U+259x
+            ].join(''),
+        });
+    }
+
+    listener(message) {
+        this.setState({ reveal: message });
+    }
+
+    flip() {
+        this.state.obfuscate = !this.state.obfuscate;
+
+        if(this.state.obfuscate) {
+            this.confuser.obfuscate(750);
+        } else {
+            this.confuser.resolve(750);
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ resolution: nextProps.reveal });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.resolution !== this.state.resolution) {
+            this.state.obfuscate = true;
+            this.confuser.setResolution(this.state.resolution, this.state.obfuscate);
+            this.confuser.obfuscate(750);
+        }
+    }
+
+    render() {
+        return (
+            <span style={{ ...this.props.style }} onClick={ this.flip.bind(this) }>
+                { this.state.reveal }
+            </span>
+        )
+    }
+}
