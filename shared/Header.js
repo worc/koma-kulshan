4ba63cf76bd9345d0464f384c198e1b5a8731ea3
worc <curtis.s.mcallister@gmail.com>
2018-02-09 14:12:27 -0800

    refactor Header to not render a new state twice when loading

--- shared/Header.js
+++ shared/Header.js
@@ -18,36 +18,52 @@ const headers = [
     'not a good place to fall into the water'
 ];
 
-const headerGenerator = getFromShuffled(headers);
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default () => {
-    const style = {
-        fontSize: '0.8rem',
-        fontVariantCaps: 'all-small-caps',
-        letterSpacing: '0.1rem',
-        padding: '1px 10px',
-        textAlign: 'justify',
-    };
+        this.headerGenerator = getFromShuffled(headers);
 
-    // this takes the place of an :after pseudo class that would trigger
-    // text-align: justify to work correctly
+        this.state = {
+            headline: ''
+        };
 
-    // after content to trigger justify:
-    // https://stackoverflow.com/a/43728929/769780
+        this.style = {
+            fontSize: '0.8rem',
+            fontVariantCaps: 'all-small-caps',
+            letterSpacing: '0.1rem',
+            padding: '1px 10px',
+            textAlign: 'justify',
+        };
 
-    // pseudo classes in react:
-    // https://stackoverflow.com/questions/28269669/css-pseudo-elements-in-react
-    const hack = {
-        display: 'inline-block',
-        width: '100%'
-    };
+        // this takes the place of an :after pseudo class that would trigger
+        // text-align: justify to work correctly
 
-    return (
-        <header style={ style }>
-            <div>
-                { headerGenerator.next().value }
-                <div style={ hack } />
-            </div>
-        </header>
-    )
+        // after content to trigger justify:
+        // https://stackoverflow.com/a/43728929/769780
+
+        // pseudo classes in react:
+        // https://stackoverflow.com/questions/28269669/css-pseudo-elements-in-react
+        this.hackStyle = {
+            display: 'inline-block',
+            width: '100%'
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ headline: this.headerGenerator.next().value });
+    }
+
+
+    render() {
+        return (
+            <header style={ this.style }>
+                <div>
+                    { this.state.headline }
+                    <div style={ this.hackStyle } />
+                </div>
+            </header>
+        )
+    }
+
 }
