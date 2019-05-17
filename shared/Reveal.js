e8f591d8fce94871a600f16750b8dfc98cdc29ed
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-05-16 17:22:06 -0700

    convert Reveal to a function component
    this didn't fix the memory leak, since there isn't a good way to force stop the reveal interval from the outside here.

--- shared/Reveal.js
+++ shared/Reveal.js
@@ -1,36 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Confuse from '../util/Confuse';
 
-export default class Reveal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            reveal: ''
-        };
+export default ({ reveal }) => {
+  const [currentReveal, setCurrentReveal] = useState()
+
+  useEffect(() => {
+    const confuser = new Confuse(message => setCurrentReveal(message), reveal, {
+      // todo let Confuse handle arrays of characters
+      // todo fix Confuse memory leak?
+      // use bamboozle npm package directly
+      characters: [
+        '▀▁▂▃▄▅▆▇█▉▊▋▌▍▎', // U+258x
+        '▐░▒▓▔▕▖▗▘▙▚▛▜▝▞▟', // U+259x
+      ].join(''),
+      exclude: '. ',
+      speed: 50,
+    })
+
+    confuser.loop(750).resolve(750)
+
+    return () => {
+      confuser.pause()
     }
 
-    componentDidMount() {
-        this.confuser = new Confuse(message => this.setState({ reveal: message }), this.props.reveal, {
-            // todo let Confuse handle arrays of characters
-            characters: [
-                '▀▁▂▃▄▅▆▇█▉▊▋▌▍▎', // U+258x
-                '▐░▒▓▔▕▖▗▘▙▚▛▜▝▞▟', // U+259x
-            ].join(''),
-            exclude: '. ',
-            speed: 50
-        });
-    }
+  }, [reveal])
 
-    componentDidUpdate(prevProps) {
-        if(prevProps.reveal !== this.props.reveal) {
-            this.confuser.setResolution(this.props.reveal);
-            this.confuser.loop(750).resolve(750);
-        }
-    }
-
-    render() {
-        return (
-            <span style={{ ...this.props.style }}>{ this.state.reveal }</span>
-        )
-    }
+  return <span>{ currentReveal }</span>
 }
