805eb4eae22b4181dca5325b098132dd5ce431d7
worc <curtis.s.mcallister@gmail.com>
2020-03-02 22:08:35 -0800

    make the new webpack config the one and only

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,60 @@
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path')
 
-module.exports = [
-    {
-        name: "server",
-        entry: "./server.js",
-        target: "node",
+const NODE_ENV = process.env.NODE_ENV
 
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "server.js"
+module.exports = {
+  mode: NODE_ENV,
+
+  devtool: NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
+
+  devServer: {
+    contentBase: 'dist/',
+    historyApiFallback: true,
+  },
+
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/preset-env', '@babel/preset-react' ],
+          },
         },
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ],
+      },
+    ]
+  },
 
-        resolve: {
-            modules: ['shared', 'node_modules']
-        },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+    }),
+    new BundleAnalyzerPlugin({
+      logLevel: 'error',
+      analyzerMode: 'static',
+      reportFilename: path.join(__dirname, `.bundle_analysis.html`),
+      openAnalyzer: false,
+    }),
+  ],
 
-        module: {
-            rules: [
-                {
-                    test: /\.m?js$/,
-                    exclude: /node_modules/,
-                    loader: "babel-loader",
-                }
-            ]
-        },
-    }, {
-        name: 'client',
-        entry: './client.js',
-
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'client.js',
-        },
-
-        resolve: {
-            modules: ['shared', 'node_modules']
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.m?js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader'
-                }
-            ]
-        }
-    }
-];
+  output: {
+    publicPath: '/',
+    filename: '[name].[hash].js',
+  },
+}
